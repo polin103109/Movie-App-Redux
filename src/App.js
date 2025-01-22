ae91edf1e1c9e226301503d39513cba8c14996f5
polin103109 <sabrinpolinece18@gmail.com>
2025-01-22 13:29:20 +0600

    updated:home and footer structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
 function App() {
-  return <div className="App">Movie app</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:imdbID" element={<MovieDetail />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="movie-logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+          alt="user"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
