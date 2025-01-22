af373b5ebd6085f6a5aa2b294f426a21d554c597
polin103109 <sabrinpolinece18@gmail.com>
2025-01-22 16:42:31 +0600

    updated:api key

---
+++ src/common/apis/MovieApikey.js
@@ -0,0 +1,1 @@
+export const APIKey = "434ed642";
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/movieApikey";
 
 function Home() {
-  return <div>Home</div>;
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await movieApi.get(
+          `?apikey=${APIKey}&s=Harry+Potter&plot=full`
+        );
+        setMovies(response.data.Search || []); // Assuming `Search` contains the list of movies
+      } catch (err) {
+        setError("Failed to fetch movies. Please try again later.");
+        console.error(err);
+      }
+    };
+
+    fetchMovies();
+  }, []); // Empty dependency array since APIKey doesn't change
+
+  return (
+    <div>
+      <div className="banner-image"></div>
+      {error ? <div>{error}</div> : <MovieListing movies={movies} />}
+    </div>
+  );
 }
 
 export default Home;
